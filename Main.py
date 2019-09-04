619f16b766ee30b5c36ebacca097d3d8229b8365
xu139411 <xuzhang2017@u.northwestern.edu>
2019-09-03 21:43:14 -0500

    Population initialization of Main.py on Xu_local_mac branch

--- Main.py
+++ Main.py
@@ -30,10 +30,32 @@ toolbox = base.Toolbox()
 #           in the file GA_control.txt.
 #           Moreover, there are parameters that have constraints:
 #           2.82 < R< 3.8; 0 < D < R-2.81821; lambda2 < lambda1 < 2 * lambda2
-#           A > B
-#           The following codes register a population of individuals that
-#           satisfy those requirements
+#           A > B.
+#           The following codes register a population of individuals.
 
+toolbox.register("attr_m", lambda: 1)
+toolbox.register("attr_gamma", random.uniform, gamma_min, gamma_max)
+toolbox.register("attr_lambda3", random.uniform, lambda3_min, lambda3_max)
+toolbox.register("attr_c", random.uniform, c_min, c_max)
+toolbox.register("attr_d", random.uniform, d_min, d_max)
+toolbox.register("attr_costheta0", random.uniform, costheta0_min,
+                 costheta0_max)
+toolbox.register("attr_n", random.uniform, n_min, n_max)
+toolbox.register("attr_beta", random.uniform, beta_min, beta_max)
+toolbox.register("attr_lambda2", random.uniform, lambda2_min, lambda2_max)
+toolbox.register("attr_B", random.uniform, B_min, B_max)
+toolbox.register("attr_R", random.uniform, R_min, R_max)
+toolbox.register("attr_D", random.uniform, D_min, D_max)
+toolbox.register("attr_lambda1", random.uniform, lambda1_min, lambda1_max)
+toolbox.register("attr_A", random.uniform, A_min, A_max)
+toolbox.register("individual", tools.initCycle, creator.Individual,
+                     (toolbox.attr_m, toolbox.attr_gamma, toolbox.attr_lambda3,
+                      toolbox.attr_c, toolbox.attr_d, toolbox.attr_costheta0,
+                      toolbox.attr_n, toolbox.attr_beta, toolbox.attr_lambda2,
+                      toolbox.attr_B, toolbox.attr_R, toolbox.attr_D,
+                      toolbox.attr_lambda1, toolbox.attr_A), n=1)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+toolbox.population(n=2)
 
 def main(checkpoint=None):
     if checkpoint:
