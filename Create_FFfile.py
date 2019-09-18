db5a19e3ce7c013e775b0392ea0bcc109ac8ab16
xu139411 <xuzhang2017@u.northwestern.edu>
2019-09-17 21:25:49 -0500

    create force field file

--- Create_FFfile.py
+++ Create_FFfile.py
@@ -1,6 +1,6 @@
 def create_fffile(individual):
     #   With the input list individual that contains all the force field
-    #   parameters, create a file with all parameters that is read by LAMMPS
+    #   parameters, create a file to be read by LAMMPS
     #   Individual has all the parameters in the following order:
-    #   m (=1), gamma, lambda3, c, d, costheta0,
-    #   n, beta, lambda2, B, R, D, lambda1 and A
+    #   [m (=1), gamma, lambda3, c, d, costheta0,
+    #   n, beta, lambda2, B, R, D, lambda1, A]
